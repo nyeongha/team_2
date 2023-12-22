a67ae1d880d37c3eded7262e3479a3619d85e947
nyeongha <mytokki0323@gmail.com>
2023-12-22 14:05:57 +0900

    js수정했어요

--- js/index.js
+++ js/index.js
@@ -1,39 +1,24 @@
 
-// $('#geonhui').click(async function(){
-//     window.location.href="./geonhui.html"
-// });
+// $(document).on("click", "#wanhui", function(){
+//     window.location.replace("./html/wanhui.html");
+//   });
 
+// $(document).on("click", "#hyoil", function(){
+//     window.location.replace("./html/hyoil.html");
+//   });
 
-// $('#minyeong').click(async function(){
-//     window.location.href="./minyeong.html"
-// });
+//   $(document).on("click", "#minyeong", function(){
+//     window.location.replace("./html/minyeong.html");
+//   });
+//   $(document).on("click", "#sehun", function(){
+//     window.location.replace("./html/sehun.html");
+//   });
+//   $(document).on("click", "#geonhui", function(){
+//     window.location.replace("./html/geonhui.html");
+//   });
 
-
-// $('#sehun').click(async function(){
-//     window.location.href="./sehun.html"
-// });
-
-// $('#hyoil').click(async function(){
-//     window.location.href="./hyoil.html"
-// });
-
-
-
-$(document).on("click", "#wanhui", function(){
-    window.location.replace("./wanhui.html");
-  });
-
-$(document).on("click", "#hyoil", function(){
-    window.location.replace("./hyoil.html");
-  });
-
-  $(document).on("click", "#minyeong", function(){
-    window.location.replace("./minyeong.html");
-  });
-  $(document).on("click", "#sehun", function(){
-    window.location.replace("./sehun.html");
-  });
-  $(document).on("click", "#geonhui", function(){
-    window.location.replace("./geonhui.html");
+  $(document).on("click", ".card-body", function(){
+    var id = $(this).attr('id');
+    window.location.replace("./html/"+id+".html");
   });
 
